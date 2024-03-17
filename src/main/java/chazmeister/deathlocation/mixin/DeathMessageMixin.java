6d33a7e1c35375dccfe15c8dc5beb54247d7e1b2
ChazMeister <8257316+ChazMeister@users.noreply.github.com>
2024-03-17 01:07:47 +0000

    Changed to another method of triggering the death location message to fix issue #2. Added hover and click actions to the displayed coordinates to copy them to the clipboard. Addresses issue #1. Incremented mod minor version.

--- src/main/java/chazmeister/deathlocation/mixin/DeathMessageMixin.java
+++ src/main/java/chazmeister/deathlocation/mixin/DeathMessageMixin.java
@@ -1,23 +1,40 @@
 package chazmeister.deathlocation.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.ChatHud;
-import net.minecraft.client.gui.screen.DeathScreen;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.network.packet.s2c.play.DeathMessageS2CPacket;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.text.HoverEvent.ItemStackContent;
+
+import java.util.Optional;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(DeathScreen.class)
+@Mixin(DeathMessageS2CPacket.class)
 public class DeathMessageMixin {
-	@Inject(method = "init()V", at = @At("HEAD"))
-	private void addDeathChatMessage(CallbackInfo info) {
+	private boolean discard = false;
 
-		MinecraftClient mc = MinecraftClient.getInstance();
-		
-		ChatHud c = mc.inGameHud.getChatHud();
-		c.addMessage(Text.literal("Death location: §cX: " + mc.player.getBlockX() + " §aY: " + mc.player.getBlockY() + " §3Z: " + mc.player.getBlockZ()));
+	@Inject(method = "apply", at = @At("HEAD"))
+	private void addDeathChatMessage(CallbackInfo info) {
+		// Seems to always run twice, so discard every second time
+		if (!discard) {
+			MinecraftClient mc = MinecraftClient.getInstance();
+			ClientPlayerEntity player = mc.player;
+
+			// Send a message to the client on death
+			player.sendMessage(
+				Text.literal("Death location: ")
+				.append(Text.literal("§cX: " + player.getBlockX() + " §aY: " + player.getBlockY() + " §3Z: " + player.getBlockZ())
+				.setStyle(Style.EMPTY
+				.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, player.getBlockX() + " " + player.getBlockY() + " " + player.getBlockZ()))
+				.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to copy location"))))));
+		}
+		discard = !discard;
 	}
 }
